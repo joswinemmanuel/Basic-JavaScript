4e3c72d161b9ae07b10a5d8e89022c9bde7affd0
Joswin Emmanuel <96587885+joswinemmanuel@users.noreply.github.com>
2022-06-16 07:12:11 +0530

    Add files via upload

---
+++ types.js
@@ -0,0 +1,25 @@
+const age=10;
+// type number
+console.log(typeof age,age);
+
+var name="Joswin Emmanuel";
+var name='Joswin Emmanuel';
+var name=`Joswin Emmanuel`;
+/* type string
+   all 3 cases possible */
+console.log(typeof name,name);
+
+const above_18=true;
+// type boolean
+console.log(typeof above_18,above_18);
+
+const percent=99.1;
+// also type number
+console.log(typeof percent,percent);
+
+let data=null;
+// type object
+console.log(typeof data,data);
+let data1;
+// type undefined
+console.log(typeof data1,data1);
