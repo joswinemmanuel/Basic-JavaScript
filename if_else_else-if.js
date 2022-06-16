01d1b9d2dc122ca1d7d9f7f371dc1b0c0ee0b0e4
Joswin Emmanuel <96587885+joswinemmanuel@users.noreply.github.com>
2022-06-16 07:12:37 +0530

    Add files via upload

---
+++ if_else_else-if.js
@@ -0,0 +1,10 @@
+const number=11;
+console.log(number);
+
+if(number<5){
+    console.log("Less than five");
+} else if(number===5){
+    console.log("Equal to five");
+} else{
+    console.log("Greater than five");
+}
