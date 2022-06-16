afd5bffc468a5c6fa0cef3379a79eb4c83c0b035
Joswin Emmanuel <96587885+joswinemmanuel@users.noreply.github.com>
2022-06-16 07:13:30 +0530

    Add files via upload

---
+++ function.js
@@ -0,0 +1,16 @@
+function add_two(num1,num2){
+    return num1+num2;
+}
+
+const number1=5;
+const number2=25;
+console.log(add_two(number1,number2));
+
+function greet(f_name,l_name,age){
+    return `${f_name} ${l_name} is ${age} years old`;
+}
+
+const first_name=`Joswin`;
+const last_name=`Emmanuel`;
+const age=19;
+console.log(greet(first_name,last_name,age));
